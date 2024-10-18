ed3316ea446fdf87fc7051ea295b3267a21dd2c3
Pesoz5 <pietropillola5@gmail.com>
2024-10-18 11:41:10 +0200

    soluzione bestoffer

--- 1/best-offer/mysolution.ml
+++ 1/best-offer/mysolution.ml
@@ -1,17 +1,17 @@
 let best_offer : int option -> int option -> int option -> int option =
-  fun of1 of2 of3 ->
-    match (of1, of2, of3) with 
-    | (None, None, None) -> None
-
-    | (Some x, None, None) 
-    | (None, Some x, None)
-    | (None, None, Some x) -> Some x
-    
-    | (Some of1, Some of2, Some of3) ->
-        if (Some of1 >= Some of2 && Some of1 >= Some of3) then Some of1
-        else 
-          if(Some of2 >= Some of1 && Some of2 >= Some of3) then Some of2
-          else Some of3
-
-        
-        
+    fun of1 of2 of3 ->
+      match (of1, of2, of3) with 
+      | (None, None, None) -> None
+  
+      | (Some x, None, None) 
+      | (None, Some x, None)
+      | (None, None, Some x) -> Some x
+      
+      | (Some of1, Some of2, Some of3) ->
+          if (Some of1 >= Some of2 && Some of1 >= Some of3) then Some of1
+          else 
+            if(Some of2 >= Some of1 && Some of2 >= Some of3) then Some of2
+            else Some of3
+  
+          
+          
