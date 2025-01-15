a98062a6e90195c6bb7218a7e47d62c115689029
Pesoz5 <pietropillola5@gmail.com>
2025-01-15 11:30:53 +0100

    solution has one

--- 3/has-one/mySolution.ml
+++ 3/has-one/mySolution.ml
@@ -1,6 +1,8 @@
-let has_one : int -> bool =
+let rec has_one : int -> bool =
   fun n -> 
     if n < 0 then failwith("negative number")
-    else
-      let str_n = string_of_int n in
-      String.exists (fun c -> c = '1' ) str_n
+    else if n == 0 then false
+
+      else 
+        if n mod 10 = 1 then true
+        else has_one (n/10)
