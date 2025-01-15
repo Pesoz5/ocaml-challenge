6fea2a35e6cac1e56c09466c3cf777e3ed7a21ab
Pesoz5 <pietropillola5@gmail.com>
2025-01-15 16:15:00 +0100

    solution string of list

---
+++ 3/string-of-list/mySolution.ml
@@ -0,0 +1,15 @@
+let string_of_list : int list -> string =
+  fun list ->
+    let rec recursiveFun list string=
+      match list with
+      | [] -> ""
+      | [x] -> string ^ string_of_int x
+      | h :: t ->
+        recursiveFun t (string ^ string_of_int h ^ ";")
+    in
+    "[" ^ (recursiveFun list "") ^ "]"
+
+
+
+    
+      
